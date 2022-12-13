921511baaa6bfa6aeaa2def52febf26fcdf916b4
DenilDenilson <jordanpreseleccion@gmail.com>
2022-12-13 18:25:52 -0500

    cambiado de mayus a minus

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:DenilDenilson/DenilDenilson.github.io.git main
+git push -f git@github.com:denildenilson/denildenilson.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
