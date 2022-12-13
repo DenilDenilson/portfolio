bc9643284b8e7fbc985449140309ab49e6deceb0
DenilDenilson <jordanpreseleccion@gmail.com>
2022-12-13 01:32:58 -0500

    desktop_1440

--- src/Components/Contactme.jsx
+++ src/Components/Contactme.jsx
@@ -4,10 +4,7 @@ import SocialNetworks from "./SocialNetworks";
 export default function Contactme() {
   return (
     <section className="mx-auto flex max-w-md flex-col items-center gap-3 lg:max-w-[832px] lg:gap-6">
-      <SectionTitles
-        extraCss="lg2:text-6xl"
-        title="Conctact me"
-      ></SectionTitles>
+      <SectionTitles extraCss="lg2:text-6xl" title="Contact me"></SectionTitles>
       <SocialNetworks
         size="48px"
         extraCss="lg:invisible lg:absolute"
